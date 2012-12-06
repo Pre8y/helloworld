23b2feff50f05913cac1d5adcd5868bbd5a059b0
Preeti Patwa <rimjhimlrl@gmail.com>
2012-12-07 01:36:51 +0530

    input handler added

--- helloworld.py
+++ helloworld.py
@@ -1,12 +1,24 @@
 import webapp2
 
 from google.appengine.api import users
-
+form = """
+	<form method="post" action="/testform">
+		<input name= "q"/>
+		<input type="submit"/>
+	</form>
+"""
 class MainPage(webapp2.RequestHandler):
 	def get(self):
-		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.out.write('Hello, Udacity')
+		#self.response.headers['Content-Type'] = 'text/plain'
+		self.response.out.write(form)
 
-	
-app = webapp2.WSGIApplication([('/', MainPage)],
+class TestHandler(webapp2.RequestHandler):
+	def post(self):
+		q = self.request.get("q")
+		self.response.out.write(q)
+	#	self.response.headers['Content-Type'] = 'text/plain'
+	#	self.response.out.write(self.request)
+		
+app = webapp2.WSGIApplication([('/', MainPage),
+				('/testform', TestHandler)],
 				debug = True)
